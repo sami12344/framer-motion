a2af813f307c5a7802570da0c2ec7146f6bb9a74
sami12344 <freelancersami16203@gmail.com>
2023-08-25 17:23:35 +0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
-import Box1 from './components/animatedBox'
-import Box3 from './components/animatedBoxThree'
-import Box2 from './components/animatedBoxTwo'
+import Box4 from './components/animatedBoxFour';
+import ContainerBox from './components/containerBox';
+import UseAnimationHook from './components/useAnimationHook';
 
 const App: React.FC = () => {
   return (
     <>
-      <Box1 />
+      {/* <Box1 />
       <Box2 />
-      <Box3 />
+      <Box3 /> */}
+      {/* <ContainerBox />
+      <Box4 /> */}
+      <UseAnimationHook />
     </>
   )
 }
