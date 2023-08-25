e2b3dcbb0b4090213f56e05e9c38547f8fdd9efc
sami12344 <freelancersami16203@gmail.com>
2023-08-25 13:00:36 +0600

    commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Box1 from './components/animatedBox'
+import Box3 from './components/animatedBoxThree'
+import Box2 from './components/animatedBoxTwo'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Box1 />
+      <Box2 />
+      <Box3 />
+    </>
+  )
+}
+
+export default App
